a1fc5dc4012210b963d79fc1a2d49e4d74bb18ee
IwishyouI <assameasyou11@gmail.com>
2024-01-29 18:52:44 +0900

    이미지가 똑바로 나오지 않는 부분 수정 (EL1025E: The collection has '74' elements, index '74' is invalid)

--- src/main/java/com/Mars/study/service/NasaMarsService.java
+++ src/main/java/com/Mars/study/service/NasaMarsService.java
@@ -14,7 +14,7 @@ public class NasaMarsService {
     public NasaResponse getRoverData() {
 
         RestTemplate rt = new RestTemplate();
-        ResponseEntity<NasaResponse> forEntity = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE", NasaResponse.class);
+        ResponseEntity<NasaResponse> forEntity = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE", NasaResponse.class);
         NasaResponse body = forEntity.getBody();
         System.out.println(body);
 
