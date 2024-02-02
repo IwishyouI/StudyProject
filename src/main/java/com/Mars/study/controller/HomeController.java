dc6e11e3f058214360ac640086bb151a19545a38
IwishyouI <assameasyou11@gmail.com>
2024-02-02 12:59:11 +0900

    Refactoring을 통한 카메라 추가 부분 정리

--- src/main/java/com/Mars/study/controller/HomeController.java
+++ src/main/java/com/Mars/study/controller/HomeController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.thymeleaf.util.StringUtils;
 
+import java.lang.reflect.InvocationTargetException;
+
 @Controller
 public class HomeController {
 
@@ -18,7 +20,7 @@ public class HomeController {
     private NasaMarsService service;
 
     @GetMapping("/")
-    public String HomeView(ModelMap modelMap, HomeDto homeDto) {
+    public String HomeView(ModelMap modelMap, HomeDto homeDto) throws InvocationTargetException, IllegalAccessException {
 
         if (StringUtils.isEmpty(homeDto.getMarsApiRoverData())) {
             homeDto.setMarsApiRoverData("Opportunity");
--- src/main/java/com/Mars/study/service/NasaMarsService.java
+++ src/main/java/com/Mars/study/service/NasaMarsService.java
@@ -8,20 +8,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.sound.midi.ShortMessage;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 @Service
 public class NasaMarsService {
 
+    private Map<String, List<String>> validCameras = new HashMap<>();
 
-    public NasaResponse getRoverData(HomeDto homeDto) {
+    public NasaMarsService() {
+        validCameras.put("Opportunity", Arrays.asList("FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"));
+        validCameras.put("Curiosity", Arrays.asList("FHAZ", "RHAZ", "MAST", "CHEMCAM", "MAHLI", "MARDI", "NAVCAM"));
+        validCameras.put("Spirit", Arrays.asList("FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"));
+    }
+
+    public NasaResponse getRoverData(HomeDto homeDto) throws InvocationTargetException, IllegalAccessException {
 
         RestTemplate rt = new RestTemplate();
         ResponseEntity<NasaResponse> forEntity = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=FHAZ", NasaResponse.class);
         NasaResponse response = forEntity.getBody();
         List<String> apiUrlEndpoints = getApiUrlEndPoints(homeDto);
-        System.out.println(response);
         List<NasaMarsPhoto> photos = new ArrayList<>();
 
         apiUrlEndpoints.stream().forEach(url -> {
@@ -34,45 +42,58 @@ public class NasaMarsService {
         return response;
     }
 
-    public List<String> getApiUrlEndPoints(HomeDto homeDto) {
+    public List<String> getApiUrlEndPoints(HomeDto homeDto) throws InvocationTargetException, IllegalAccessException {
         List<String> urls = new ArrayList<>();
 
-        if (Boolean.TRUE.equals(homeDto.getCHEMCAM())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=CHEMCAM");
+        Method[] methods = homeDto.getClass().getMethods();
+
+        for (Method method : methods) {
+
+            if (method.getName().indexOf("get") > -1 && Boolean.TRUE.equals(method.invoke(homeDto))) {
+                String cameraName = method.getName().split("get")[1].toUpperCase();
+                if (validCameras.get(homeDto.getMarsApiRoverData()).contains(cameraName)) {
+                    urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=" + cameraName);
+
+                }
+            }
         }
 
-        if (Boolean.TRUE.equals(homeDto.getRHAZ())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=RHAZ");
-        }
-
-        if (Boolean.TRUE.equals(homeDto.getMAST())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=MAST");
-        }
-
-        if (Boolean.TRUE.equals(homeDto.getFHAZ())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=FHAZ");
-        }
-
-        if (Boolean.TRUE.equals(homeDto.getMAHLI())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=MAHLI");
-        }
-
-        if (Boolean.TRUE.equals(homeDto.getMARDI())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=MARDI");
-        }
-
-        if (Boolean.TRUE.equals(homeDto.getNAVCAM())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=NAVCAM");
-        }
-
-        if (Boolean.TRUE.equals(homeDto.getPANCAM())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=PANCAM");
-        }
-
-        if (Boolean.TRUE.equals(homeDto.getMINITES())) {
-            urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=MINITES");
-        }
 
         return urls;
     }
 }
+//  if (Boolean.TRUE.equals(homeDto.getCHEMCAM())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=CHEMCAM");
+//          }
+//
+//          if (Boolean.TRUE.equals(homeDto.getRHAZ())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=RHAZ");
+//          }
+//
+//          if (Boolean.TRUE.equals(homeDto.getMAST())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=MAST");
+//          }
+//
+//          if (Boolean.TRUE.equals(homeDto.getFHAZ())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=FHAZ");
+//          }
+//
+//          if (Boolean.TRUE.equals(homeDto.getMAHLI())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=MAHLI");
+//          }
+//
+//          if (Boolean.TRUE.equals(homeDto.getMARDI())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=MARDI");
+//          }
+//
+//          if (Boolean.TRUE.equals(homeDto.getNAVCAM())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=NAVCAM");
+//          }
+//
+//          if (Boolean.TRUE.equals(homeDto.getPANCAM()) && !"curiosity".equalsIgnoreCase(homeDto.getMarsApiRoverData())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=PANCAM");
+//          }
+//
+//          if (Boolean.TRUE.equals(homeDto.getMINITES()) && !"curiosity".equalsIgnoreCase(homeDto.getMarsApiRoverData())) {
+//          urls.add("https://api.nasa.gov/mars-photos/api/v1/rovers/" + homeDto.getMarsApiRoverData() + "/photos?sol=" + homeDto.getMarsSol() + "&api_key=62FQB9dDvEpei7mKgt7l8xQeB9aSS4efuTK0FrjE" + "&camera=MINITES");
+//          }
